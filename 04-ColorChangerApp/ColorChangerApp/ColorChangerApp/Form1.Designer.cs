f77e24c27bbb48bcf4fc177815d3221ea43a3a9f
nkturtas@gmx.com <nkturtas@gmx.com>
2026-01-30 12:28:57 +0200

    Add 04-ColorChangerApp

---
+++ 04-ColorChangerApp/ColorChangerApp/ColorChangerApp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ColorChangerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeColor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChangeColor
+            // 
+            this.btnChangeColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangeColor.Location = new System.Drawing.Point(289, 228);
+            this.btnChangeColor.Name = "btnChangeColor";
+            this.btnChangeColor.Size = new System.Drawing.Size(208, 57);
+            this.btnChangeColor.TabIndex = 0;
+            this.btnChangeColor.Text = "Change Color";
+            this.btnChangeColor.UseVisualStyleBackColor = true;
+            this.btnChangeColor.Click += new System.EventHandler(this.btnChangeColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChangeColor);
+            this.Name = "Form1";
+            this.Text = "Color Changer App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeColor;
+    }
+}
+
