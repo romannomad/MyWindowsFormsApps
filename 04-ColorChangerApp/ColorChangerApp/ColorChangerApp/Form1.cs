a0a4e8f507b4d334128b2b22322320a42695f93f
Roman <63406202+romannomad@users.noreply.github.com>
2026-01-30 13:24:58 +0200

    Add files via upload

---
+++ 04-ColorChangerApp/ColorChangerApp/ColorChangerApp/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorChangerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeColor_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            this.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+        }
+    }
+}
