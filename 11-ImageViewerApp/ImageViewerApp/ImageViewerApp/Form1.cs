7f20fda50c945f2a9c620fbb587a9fc88ecc19a8
Roman <63406202+romannomad@users.noreply.github.com>
2026-02-06 12:56:06 +0200

    Add files via upload

---
+++ 11-ImageViewerApp/ImageViewerApp/ImageViewerApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif; ";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(dialog.FileName);
+            }
+        }
+    }
+}
