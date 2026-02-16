0e368540e05eb2febd7bd9057bc0362eb30d7854
Roman <63406202+romannomad@users.noreply.github.com>
2026-02-16 14:20:20 +0200

    Add files via upload

---
+++ 20-CharacterCounterApp/20-CharacterCounterApp/20-CharacterCounterApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20_CharacterCounterApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateCharacterCount();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            UpdateCharacterCount();
+        }
+
+        private void UpdateCharacterCount()
+        {
+            {
+                int count = txtInput.Text.Length;
+                lblCount.Text = $"Characters: {count}";
+            }
+        }
+    }
+}
