80dfa7d725bf05549738b31dc9e4f9e19cb94026
Roman <vipshop@gmx.com>
2026-03-01 10:56:01 +0200

    InventoryManagementApp

--- 27-InventoryManagementApp/InventoryManagementApp/InventoryManagementApp/Form1.Designer.cs
+++ 27-InventoryManagementApp/InventoryManagementApp/InventoryManagementApp/Form1.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInventoryName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.inventoryGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblInventoryName
+            // 
+            this.lblInventoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInventoryName.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblInventoryName.Location = new System.Drawing.Point(12, 13);
+            this.lblInventoryName.Name = "lblInventoryName";
+            this.lblInventoryName.Size = new System.Drawing.Size(1012, 74);
+            this.lblInventoryName.TabIndex = 0;
+            this.lblInventoryName.Text = "Inventory Management Studio";
+            this.lblInventoryName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(340, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // inventoryGridView
+            // 
+            this.inventoryGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.inventoryGridView.BackgroundColor = System.Drawing.Color.White;
+            this.inventoryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inventoryGridView.Location = new System.Drawing.Point(12, 164);
+            this.inventoryGridView.Name = "inventoryGridView";
+            this.inventoryGridView.RowHeadersWidth = 51;
+            this.inventoryGridView.RowTemplate.Height = 24;
+            this.inventoryGridView.Size = new System.Drawing.Size(1028, 354);
+            this.inventoryGridView.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // InventoryManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1046, 520);
+            this.ClientSize = new System.Drawing.Size(1053, 530);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inventoryGridView);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblInventoryName);
             this.Name = "InventoryManagement";
             this.Text = "Inventory Management Studio";
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInventoryName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView inventoryGridView;
+        private System.Windows.Forms.Button button1;
     }
 }
 
