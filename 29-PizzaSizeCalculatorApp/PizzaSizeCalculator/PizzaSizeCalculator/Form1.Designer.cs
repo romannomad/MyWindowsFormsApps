84ece2c54d711649b4e5d1198d90830d5bfb9f44
Roman <vipshop@gmx.com>
2026-03-05 15:26:25 +0200

    PizzaSizeCalculator
    Shows the 3 prices of the pizza.

---
+++ 29-PizzaSizeCalculatorApp/PizzaSizeCalculator/PizzaSizeCalculator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PizzaSizeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxSize = new System.Windows.Forms.GroupBox();
+            this.radioSmall = new System.Windows.Forms.RadioButton();
+            this.radioMedium = new System.Windows.Forms.RadioButton();
+            this.radioLarge = new System.Windows.Forms.RadioButton();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.groupBoxSize.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxSize
+            // 
+            this.groupBoxSize.Controls.Add(this.labelPrice);
+            this.groupBoxSize.Controls.Add(this.radioLarge);
+            this.groupBoxSize.Controls.Add(this.radioMedium);
+            this.groupBoxSize.Controls.Add(this.radioSmall);
+            this.groupBoxSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxSize.Location = new System.Drawing.Point(21, 12);
+            this.groupBoxSize.Name = "groupBoxSize";
+            this.groupBoxSize.Size = new System.Drawing.Size(806, 268);
+            this.groupBoxSize.TabIndex = 0;
+            this.groupBoxSize.TabStop = false;
+            this.groupBoxSize.Text = "Pizza Size";
+            // 
+            // radioSmall
+            // 
+            this.radioSmall.AutoSize = true;
+            this.radioSmall.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioSmall.Location = new System.Drawing.Point(75, 78);
+            this.radioSmall.Name = "radioSmall";
+            this.radioSmall.Size = new System.Drawing.Size(82, 29);
+            this.radioSmall.TabIndex = 0;
+            this.radioSmall.TabStop = true;
+            this.radioSmall.Text = "Small";
+            this.radioSmall.UseVisualStyleBackColor = true;
+            this.radioSmall.CheckedChanged += new System.EventHandler(this.radioSmall_CheckedChanged);
+            // 
+            // radioMedium
+            // 
+            this.radioMedium.AutoSize = true;
+            this.radioMedium.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioMedium.Location = new System.Drawing.Point(242, 78);
+            this.radioMedium.Name = "radioMedium";
+            this.radioMedium.Size = new System.Drawing.Size(103, 29);
+            this.radioMedium.TabIndex = 1;
+            this.radioMedium.TabStop = true;
+            this.radioMedium.Text = "Medium";
+            this.radioMedium.UseVisualStyleBackColor = true;
+            this.radioMedium.CheckedChanged += new System.EventHandler(this.radioMedium_CheckedChanged);
+            // 
+            // radioLarge
+            // 
+            this.radioLarge.AutoSize = true;
+            this.radioLarge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioLarge.Location = new System.Drawing.Point(404, 78);
+            this.radioLarge.Name = "radioLarge";
+            this.radioLarge.Size = new System.Drawing.Size(83, 29);
+            this.radioLarge.TabIndex = 2;
+            this.radioLarge.TabStop = true;
+            this.radioLarge.Text = "Large";
+            this.radioLarge.UseVisualStyleBackColor = true;
+            this.radioLarge.CheckedChanged += new System.EventHandler(this.radioLarge_CheckedChanged);
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(570, 78);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(0, 32);
+            this.labelPrice.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 450);
+            this.Controls.Add(this.groupBoxSize);
+            this.Name = "Form1";
+            this.Text = "Pizza Size Calculator";
+            this.groupBoxSize.ResumeLayout(false);
+            this.groupBoxSize.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxSize;
+        private System.Windows.Forms.RadioButton radioLarge;
+        private System.Windows.Forms.RadioButton radioMedium;
+        private System.Windows.Forms.RadioButton radioSmall;
+        private System.Windows.Forms.Label labelPrice;
+    }
+}
+
---
+++ 29-PizzaSizeCalculatorApp/PizzaSizeCalculator/PizzaSizeCalculator/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaSizeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioSmall_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioSmall.Checked)
+            {
+                UpdatePrice(5.00m);
+            }
+        }
+
+        private void radioMedium_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioMedium.Checked)
+            {
+                UpdatePrice(8.50m);
+            }
+        }
+
+        private void radioLarge_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioLarge.Checked)
+            {
+                UpdatePrice(12.00m);
+            }
+        }
+
+        private void UpdatePrice(decimal price)
+        {
+            labelPrice.Text = $"Price: ${price:F2}";
+        }
+    }
+}
