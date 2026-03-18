de8695f1619f983eee55518d9b0311ebcaa8d800
Roman <vipshop@gmx.com>
2026-03-18 13:04:02 +0200

    MyNameIsApp
    Button text changed to bigger and bold.

--- 01-MyNameIsApp/MyNameIs/MyNameIs/Form1.Designer.cs
+++ 01-MyNameIsApp/MyNameIs/MyNameIs/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // btn_name
             // 
-            this.btn_name.Location = new System.Drawing.Point(318, 187);
+            this.btn_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_name.Location = new System.Drawing.Point(298, 189);
             this.btn_name.Name = "btn_name";
-            this.btn_name.Size = new System.Drawing.Size(155, 49);
+            this.btn_name.Size = new System.Drawing.Size(232, 56);
             this.btn_name.TabIndex = 0;
             this.btn_name.Text = "Click the button";
             this.btn_name.UseVisualStyleBackColor = true;
@@ -45,9 +46,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(253, 114);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 112);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(333, 16);
+            this.label1.Size = new System.Drawing.Size(459, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Click the button below to find out my name and what I do";
             // 
